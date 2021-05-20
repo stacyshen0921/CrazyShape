278bb2ccb69e5a034e7e2948222dd68fd76ea4f5
stacyshen0921 <irisssssaine@gmail.com>
2021-05-20 21:53:29 +0800

    2021上機考v0_(沈安妮)

---
+++ app/src/main/java/tw/edu/pu/s410819471/cra4yshape/MainActivity.kt
@@ -0,0 +1,27 @@
+package tw.edu.pu.s410819471.cra4yshape
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(baseContext, "作者：沈安妮", Toast.LENGTH_LONG).show()
+
+        imgNext.setOnLongClickListener(object : View.OnLongClickListener {
+            override fun onLongClick(p0: View?): Boolean {
+                intent = Intent(this@MainActivity, GameActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        })
+    }
+}
